a710bf31e7071995fb193fd3bafc78ca57012d86
Daniil Chikish <daniil.chikish@gmail.com>
2025-04-13 22:27:41 +0300

    + add breakable crates

--- RSG_TestTaskProject/Assets/Content/Features/AIModule/Scripts/Entity/EntityBehaviours/IdleEntityBehaviour.cs
+++ RSG_TestTaskProject/Assets/Content/Features/AIModule/Scripts/Entity/EntityBehaviours/IdleEntityBehaviour.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 
-namespace Content.Features.AIModule.Scripts.Entity.EntityBehaviours {
-    public class IdleEntityBehaviour : IEntityBehaviour {
+namespace Content.Features.AIModule.Scripts.Entity.EntityBehaviours
+{
+    public class IdleEntityBehaviour : IEntityBehaviour
+    {
         private EntityContext _entityContext;
 
         public event Action OnBehaviorEnd;
@@ -9,9 +11,11 @@ namespace Content.Features.AIModule.Scripts.Entity.EntityBehaviours {
         public void InitContext(EntityContext entityContext) =>
             _entityContext = entityContext;
 
-        public void Start() {
-            _entityContext.EntityAnimator.SetIsAttacking(false);
-            _entityContext.NavMeshAgent.ResetPath();
+        public void Start()
+        {
+            _entityContext.EntityAnimator?.SetIsAttacking(false);
+            if (_entityContext.NavMeshAgent)
+                _entityContext.NavMeshAgent.ResetPath();
         }
 
         public void Process() { }
